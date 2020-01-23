b0a4372fe1232ae19aa21173c9eefba1fd744348
guthrie5 <54378586+guthrie5@users.noreply.github.com>
2020-01-23 10:34:30 -0500

    More things done today

---
+++ Book 1/Chapter1/Types/Types/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Michael";
+
+            int grade = 99;
+
+            double gradePlus = 99.12;
+
+            char gradeLetter = 'A';
+
+            Console.WriteLine($"{firstName} got a {grade} on his test. That puts his overall grade to {gradePlus} or he has an {gradeLetter} in the class.");
+        }
+    }
+}
