3486eb3238a2c1ae9621640c52458746bd276af6
guthrie5 <54378586+guthrie5@users.noreply.github.com>
2020-01-28 10:15:15 -0500

    Today's work

---
+++ Book 1/Chapter2/ForLoopAndIfStatements/ForLoopAndIfStatements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace lecture5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testGrade = 77;
+
+            if (testGrade >= 90)
+            {
+                Console.WriteLine($"You got a {testGrade} on the test, which is an A");
+            }
+
+            else
+            {
+                Console.WriteLine($"You got a {testGrade} on the test, which is a failing grade.");
+            }
+
+
+
+            for (int k = 0; k <= 100; k += 1)
+            {
+                if (k >= 90 == k <= 95)
+                {
+                    Console.WriteLine(k);
+                }
+            }
+
+            string[] Students = new string[]
+            {
+                "Samantha", "Carol", "Carrey"
+            };
+
+            foreach (string names in Students)
+            {
+                Console.WriteLine(names);
+            }
+
+            //int finalGrade = 92;
+
+            //if (finalGrade >= 90)
+            //{
+            //    Console.WriteLine($"your grade in the class is {finalGrade}, and is an A");
+            //}
+            //if (finalGrade < 90)
+            //{
+            //    Console.WriteLine($"your grade in the class is {finalGrade}, and you do not pass the class.");
+            //}
+        }
+    }
+}
