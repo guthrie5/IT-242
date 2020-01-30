f1b090962ad44a242a7d3f2650b7b5777c2b6323
guthrie5 <54378586+guthrie5@users.noreply.github.com>
2020-01-30 10:53:26 -0500

    Work Today

---
+++ Book 1/Chapter4/PlanetSpaceships/PlanetSpaceships/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanetSpaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> newplanetList = new List<string>() { "Neptune", "Uranus" };
+
+            newplanetList.AddRange(planetList);
+
+            newplanetList.Add("Pluto");
+
+            newplanetList.GetRange()
+        }
+    }
+}
