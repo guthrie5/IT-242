f9351ff3c52a98407e942da6e74eb5c013330574
guthrie5 <54378586+guthrie5@users.noreply.github.com>
2020-02-20 10:43:47 -0500

    Work Done Today

---
+++ Book 1/Chapter6/UrbanPlanner/UrbanPlanner/Building.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+        private string Designer { get; set; }
+
+        private DateTime DateConstructed { get; set; }
+
+        private string Address { get; set; }
+
+        private string Owner { get; set; }
+
+        public int Stories;
+
+        public double Width;
+
+        public double Depth;
+
+        public double Volume {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+        public class Constructor
+        {
+            public Constructor (string Address)
+            {
+                
+            }
+        }
+    }
+}
