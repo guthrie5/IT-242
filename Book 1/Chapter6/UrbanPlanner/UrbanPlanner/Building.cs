7658a1c404ea462a72a1aaf73d490613f11de31d
guthrie5 <54378586+guthrie5@users.noreply.github.com>
2020-02-25 10:11:48 -0500

    Update to Chapter 6

--- Book 1/Chapter6/UrbanPlanner/UrbanPlanner/Building.cs
+++ Book 1/Chapter6/UrbanPlanner/UrbanPlanner/Building.cs
@@ -6,33 +6,53 @@ namespace UrbanPlanner
 {
     class Building
     {
-        private string Designer { get; set; }
+        private string DesignerName { get; set; }
 
         private DateTime DateConstructed { get; set; }
 
-        private string Address { get; set; }
+        private string BuildingAddress { get; set; }
 
-        private string Owner { get; set; }
+        private string BuildingOwner { get; set; }
 
-        public int Stories;
+        public int NumberOfStories { get; set; }
 
-        public double Width;
+        public double Width { get; set; }
 
-        public double Depth;
+        public double Depth { get; set; }
 
         public double Volume {
             get
             {
-                return Width * Depth * (3 * Stories);
+                return Width * Depth * (3 * NumberOfStories);
             }
+
+            
         }
 
-        public class Constructor
+        public Building(string address, string designer)
         {
-            public Constructor (string Address)
-            {
-                
-            }
+            BuildingAddress = address;
+            DesignerName = designer;
+        }
+
+        public void Construct()
+        {
+            DateConstructed = DateTime.Now;
+        }
+
+        public void Purchase (string buyer)
+        {
+            BuildingOwner = buyer;
+        }
+
+        public void DisplayBuildingInfo()
+        {
+            Console.WriteLine(BuildingAddress);
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Designed by: " + DesignerName);
+            Console.WriteLine("Constructed on " + DateConstructed);
+            Console.WriteLine("Owned by " + BuildingOwner);
+            Console.WriteLine(Volume + "cubic meters of space");
         }
     }
 }
--- Book 1/Chapter6/UrbanPlanner/UrbanPlanner/Program.cs
+++ Book 1/Chapter6/UrbanPlanner/UrbanPlanner/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace UrbanPlanner
 {
@@ -6,7 +8,38 @@ namespace UrbanPlanner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Blarg");
+            Building MCTC = new Building("1 Mountwest Way", "Frank Lloyd Wright")
+            {
+                Width = 100,
+                Depth = 200,
+                NumberOfStories = 4,
+            };
+            
+            Building CapitalBuilding = new Building("412 8th Street", "Renzo Piano")
+            {
+                Width = 1000,
+                Depth = 500,
+                NumberOfStories = 6,
+            };
+
+            Building MyHouse = new Building("1211 Kanawha Terrace", "Michael Guthrie")
+            {
+                Width = 13.5,
+                Depth = 13.5,
+                NumberOfStories = 1,
+
+            };
+
+            MCTC.Construct();
+            CapitalBuilding.Construct();
+            MyHouse.Construct();
+            MCTC.Purchase("Russell");
+            CapitalBuilding.Purchase("Alex");
+            MyHouse.Purchase("Michael");
+
+            MCTC.DisplayBuildingInfo();
+            CapitalBuilding.DisplayBuildingInfo();
+            MyHouse.DisplayBuildingInfo();
         }
     }
 }
